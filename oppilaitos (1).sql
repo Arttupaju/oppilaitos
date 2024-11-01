f518100e24b37eb546d90cb14124453f1b48dc67
Arttupaju <128782238+Arttupaju@users.noreply.github.com>
2024-11-01 11:47:48 +0200

    Add files via upload

---
+++ oppilaitos (1).sql
@@ -0,0 +1,233 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 01, 2024 at 10:47 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `oppilaitos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kurssikirjautumiset`
+--
+
+CREATE TABLE `kurssikirjautumiset` (
+  `tunnus` int(11) NOT NULL,
+  `opiskelija_id` int(11) DEFAULT NULL,
+  `kurssi_id` int(11) DEFAULT NULL,
+  `kirjautumispaiva` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kurssikirjautumiset`
+--
+
+INSERT INTO `kurssikirjautumiset` (`tunnus`, `opiskelija_id`, `kurssi_id`, `kirjautumispaiva`) VALUES
+(1, 1, 1, '2024-10-31 09:18:50'),
+(2, 2, 1, '2024-10-31 09:18:50'),
+(4, 1, 3, '2024-10-31 09:18:50'),
+(5, 2, 3, '2024-10-31 09:18:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kurssit`
+--
+
+CREATE TABLE `kurssit` (
+  `tunnus` int(11) NOT NULL,
+  `nimi` varchar(100) NOT NULL,
+  `kuvaus` text DEFAULT NULL,
+  `alkupaiva` date NOT NULL,
+  `loppupaiva` date NOT NULL,
+  `opettaja_id` int(11) DEFAULT NULL,
+  `tila_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kurssit`
+--
+
+INSERT INTO `kurssit` (`tunnus`, `nimi`, `kuvaus`, `alkupaiva`, `loppupaiva`, `opettaja_id`, `tila_id`) VALUES
+(1, 'Matematiikan perusteet', 'Johdanto matematiikan perusteisiin.', '2024-01-15', '2024-05-30', 1, 1),
+(2, 'Fysiikan jatkokurssi', 'Syventävä fysiikan kurssi.', '2024-02-01', '2024-06-15', 2, 2),
+(3, 'Kemia ja yhteiskunta', 'Kemian rooli yhteiskunnassa.', '2024-03-01', '2024-07-01', 3, 3),
+(4, 'Kurssi', 'Kurssilla kursseillaan', '2024-01-01', '2024-02-02', 2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `opettajat`
+--
+
+CREATE TABLE `opettajat` (
+  `tunnusnumero` int(11) NOT NULL,
+  `etunimi` varchar(50) NOT NULL,
+  `sukunimi` varchar(50) NOT NULL,
+  `aine` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `opettajat`
+--
+
+INSERT INTO `opettajat` (`tunnusnumero`, `etunimi`, `sukunimi`, `aine`) VALUES
+(1, 'Maija', 'Laakso', 'Matematiikka'),
+(2, 'Pekka', 'Ahonen', 'Fysiikka'),
+(3, 'Liisa', 'Heikkinen', 'Kemian'),
+(4, 'Elli', 'Linna', 'Pseudo tieteet');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `opiskelijat`
+--
+
+CREATE TABLE `opiskelijat` (
+  `opiskelijanumero` int(11) NOT NULL,
+  `etunimi` varchar(50) NOT NULL,
+  `sukunimi` varchar(50) NOT NULL,
+  `syntymapaiva` date NOT NULL,
+  `vuosikurssi` int(11) DEFAULT NULL CHECK (`vuosikurssi` between 1 and 3)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `opiskelijat`
+--
+
+INSERT INTO `opiskelijat` (`opiskelijanumero`, `etunimi`, `sukunimi`, `syntymapaiva`, `vuosikurssi`) VALUES
+(1, 'Matti', 'Virtanen', '2002-05-10', 1),
+(2, 'Anna', 'Korhonen', '2001-03-22', 2),
+(4, 'Timo', 'Jalonen', '1010-10-10', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tilat`
+--
+
+CREATE TABLE `tilat` (
+  `tunnus` int(11) NOT NULL,
+  `nimi` varchar(50) NOT NULL,
+  `kapasiteetti` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tilat`
+--
+
+INSERT INTO `tilat` (`tunnus`, `nimi`, `kapasiteetti`) VALUES
+(1, 'A101', 30),
+(2, 'B202', 25),
+(3, 'C303', 20);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kurssikirjautumiset`
+--
+ALTER TABLE `kurssikirjautumiset`
+  ADD PRIMARY KEY (`tunnus`),
+  ADD KEY `opiskelija_id` (`opiskelija_id`),
+  ADD KEY `kurssi_id` (`kurssi_id`);
+
+--
+-- Indexes for table `kurssit`
+--
+ALTER TABLE `kurssit`
+  ADD PRIMARY KEY (`tunnus`),
+  ADD KEY `opettaja_id` (`opettaja_id`),
+  ADD KEY `tila_id` (`tila_id`);
+
+--
+-- Indexes for table `opettajat`
+--
+ALTER TABLE `opettajat`
+  ADD PRIMARY KEY (`tunnusnumero`);
+
+--
+-- Indexes for table `opiskelijat`
+--
+ALTER TABLE `opiskelijat`
+  ADD PRIMARY KEY (`opiskelijanumero`);
+
+--
+-- Indexes for table `tilat`
+--
+ALTER TABLE `tilat`
+  ADD PRIMARY KEY (`tunnus`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kurssikirjautumiset`
+--
+ALTER TABLE `kurssikirjautumiset`
+  MODIFY `tunnus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `kurssit`
+--
+ALTER TABLE `kurssit`
+  MODIFY `tunnus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `opettajat`
+--
+ALTER TABLE `opettajat`
+  MODIFY `tunnusnumero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `opiskelijat`
+--
+ALTER TABLE `opiskelijat`
+  MODIFY `opiskelijanumero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tilat`
+--
+ALTER TABLE `tilat`
+  MODIFY `tunnus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kurssikirjautumiset`
+--
+ALTER TABLE `kurssikirjautumiset`
+  ADD CONSTRAINT `kurssikirjautumiset_ibfk_1` FOREIGN KEY (`opiskelija_id`) REFERENCES `opiskelijat` (`opiskelijanumero`) ON DELETE CASCADE,
+  ADD CONSTRAINT `kurssikirjautumiset_ibfk_2` FOREIGN KEY (`kurssi_id`) REFERENCES `kurssit` (`tunnus`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `kurssit`
+--
+ALTER TABLE `kurssit`
+  ADD CONSTRAINT `kurssit_ibfk_1` FOREIGN KEY (`opettaja_id`) REFERENCES `opettajat` (`tunnusnumero`) ON DELETE SET NULL,
+  ADD CONSTRAINT `kurssit_ibfk_2` FOREIGN KEY (`tila_id`) REFERENCES `tilat` (`tunnus`) ON DELETE SET NULL;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
